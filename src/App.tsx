c32ecf0146ccda44dafa57dac845f50afdcda144
Morgan <morgan.espindola@gmail.com>
2025-09-26 08:08:55 -0300

    Area Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import * as Styles from "./App.styles";
 
 const App = () => {
-  return <Styles.Container>...</Styles.Container>;
+  return (
+    <Styles.Container>
+      <Styles.Area>.....</Styles.Area>
+    </Styles.Container>
+  );
 };
 
 export default App;
