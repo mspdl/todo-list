c3240493b64681cd61c022ecb11d4180aa19bfad
Morgan <morgan.espindola@gmail.com>
2025-09-28 18:12:02 -0300

    Styled Task Item Component

--- src/components/TaskItem/styles.ts
+++ src/components/TaskItem/styles.ts
@@ -1,5 +1,20 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  
+  display: flex;
+  background-color: #20212c;
+  padding: 10px;
+  border-radius: 10px;
+  align-items: center;
+  margin-bottom: 10px;
+
+  input {
+    width: 25px;
+    height: 25px;
+    margin-right: 5px;
+  }
+
+  label {
+    color: #ccc;
+  }
 `;
